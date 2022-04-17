74fb022adb488c23a1ca7def45670575c9630b04
Th3-Wizard <103869920+Th3-Wizard@users.noreply.github.com>
2022-04-17 08:13:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'USER TELEGRAM SIN @'
+        tl_admin_user = 'EL_Wizard'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
